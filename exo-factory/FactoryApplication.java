9fb339ec48cabc9f15af5ea82474a7ee1bed30d7
Jérémy <jeremy.perrouault@ascadis.fr>
2025-01-21 10:17:08 +0000

    Nettoyage

--- exo-factory/FactoryApplication.java
+++ exo-factory/FactoryApplication.java
@@ -28,49 +28,5 @@ public class FactoryApplication {
         lst.add("1");
 
         System.out.println(lst);
-
-        P p = new P();
-
-        Runnable task = () -> {
-            for (int i = 0; i < 10_000; i++) {
-                p.increment();
-            }
-        };
-
-        ExecutorService pool = Executors.newFixedThreadPool(2);
-
-        // Thread t1 = new Thread(task);
-        // Thread t2 = new Thread(task);
-        long start = System.nanoTime();
-        
-        Future<?> t1 = pool.submit(task);
-        Future<?> t2 = pool.submit(task);
-        // t1.start();
-        // t2.start();
-        
-        t1.get();
-        t2.get();
-
-        // t1.join();
-        // t2.join();
-
-        long end = System.nanoTime();
-
-        System.out.println((end - start) / (float)1_000_000);
-        System.out.println(p.x.get());
-
-        pool.shutdownNow();
-    }
-
-    public static class P {
-        // private int x;
-        private AtomicInteger x = new AtomicInteger(0);
-
-        // public synchronized void increment() {
-        //     x++;
-        // }
-        public void increment() {
-            x.incrementAndGet();
-        }
     }
 }
