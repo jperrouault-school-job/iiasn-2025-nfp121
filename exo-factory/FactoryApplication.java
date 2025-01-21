41ba09e7129de13f60e218f8accd76edee2e8e3b
Jérémy <jeremy.perrouault@ascadis.fr>
2025-01-21 10:17:23 +0000

    Nettoyage

--- exo-factory/FactoryApplication.java
+++ exo-factory/FactoryApplication.java
@@ -1,9 +1,5 @@
 import java.util.Collection;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class FactoryApplication {
     /*
