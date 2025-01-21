1124763f26411d46a78c77b518b731a2ba89bb1b
Jérémy <jeremy.perrouault@ascadis.fr>
2025-01-21 08:52:03 +0000

    Nettoyage

--- exo-singleton/SingletonApplication.java
+++ exo-singleton/SingletonApplication.java
@@ -1,7 +1,15 @@
+import java.lang.reflect.Constructor;
+
 public class SingletonApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SingletonModel singleton1 = SingletonModel.getInstance();
-        SingletonModel singleton2 = SingletonModel.getInstance();
+        // SingletonModel singleton2 = SingletonModel.getInstance();
+
+        Constructor<SingletonModel> ctor = SingletonModel.class.getDeclaredConstructor();
+
+        ctor.setAccessible(true);
+
+        SingletonModel singleton2 = ctor.newInstance();
 
         if (singleton1 == singleton2) {
             System.out.println("C PAREIL");
