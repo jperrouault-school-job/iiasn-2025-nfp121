1bada3c60dab85a2e6c4d1907896918385f67dfe
Jérémy <jeremy.perrouault@ascadis.fr>
2025-01-22 13:36:03 +0000

    JOUR 3 - PRENOMS PARCE QU'ILS ONT LA FLEMME

---
+++ rappels-java/FonctionnelleApplication.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class FonctionnelleApplication {
+    public static void main(String[] args) {
+        List<String> prenoms = List.of("Jérémy", "Evann", "Maxence", "Léo-Sélim", "Nathan", "Maxence", "Marine", "Evan");
+    
+        
+        // Afficher QUE les prénoms qui commencent par la lettre E
+    }
+}
