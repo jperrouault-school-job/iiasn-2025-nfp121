1a7b1fcbbc9f47f640d13c72d9be4caa3eef1175
Jérémy <jeremy.perrouault@ascadis.fr>
2025-01-22 14:05:27 +0000

    JOUR 3 - RAPPELS FONCTIONNELLE

--- rappels-java/FonctionnelleApplication.java
+++ rappels-java/FonctionnelleApplication.java
@@ -4,7 +4,22 @@ public class FonctionnelleApplication {
     public static void main(String[] args) {
         List<String> prenoms = List.of("Jérémy", "Evann", "Maxence", "Léo-Sélim", "Nathan", "Maxence", "Marine", "Evan");
     
-        
         // Afficher QUE les prénoms qui commencent par la lettre E
+        prenoms.stream()
+            // .filter((arg) -> {
+            //     return arg.startsWith("E");
+            // })
+            .filter(arg -> arg.startsWith("E"))
+
+            // .map(prenom -> prenom.toUpperCase())
+            .map(String::toUpperCase)
+
+            // .forEach(prenom -> {
+            //     System.out.println(prenom);
+            // })
+
+            // .forEach(prenom -> System.out.println(prenom))
+            .forEach(System.out::println)
+        ;
     }
 }
